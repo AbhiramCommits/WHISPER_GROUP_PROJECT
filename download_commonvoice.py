2ed1a8bd8092fb680e66faa2621f2d6d44aa82bd
Shriansh Chari <30420527+shrianshChari@users.noreply.github.com>
2026-04-29 16:17:10 -0700

    Use the env file

--- download_commonvoice.py
+++ download_commonvoice.py
@@ -1,7 +1,10 @@
 from datacollective import download_dataset
-import datacollective
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 dataset_id = 'cmndapwry02jnmh07dyo46mot'
 # print(get_dataset_details(dataset_id))
-dataset_path = download_dataset(dataset_id, download_directory='cv-data', show_progress=True, overwrite_existing=False)
+dataset_path = download_dataset(dataset_id, show_progress=True, overwrite_existing=False)
 print(dataset_path)
